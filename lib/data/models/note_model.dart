c50e74183e67681c5477c6e33c2360fdde1d3d17
fanes-setiawan <fanessetiawan.1401@gmail.com>
2023-07-24 22:41:06 +0700

    Create a data model of the Note class

---
+++ lib/data/models/note_model.dart
@@ -0,0 +1,12 @@
+class Note {
+  final int? id;
+  final String title;
+  final String content;
+  final String createdTime;
+  Note({
+    this.id,
+    required this.title,
+    required this.content,
+    required this.createdTime,
+  });
+}
